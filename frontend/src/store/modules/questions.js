8b62c438493d4a66fd229c7663e8fdc333ab7b13
gao.ling <gao.ling@ctrip.com>
2019-01-08 18:15:47 +0800

    fix

---
+++ frontend/src/store/modules/questions.js
@@ -0,0 +1,33 @@
+
+const state={
+    questionList:[
+        {qid:'',
+        qname:'',
+        qanswer:[],
+        },
+
+    ],
+    answer:[
+        {qid:'',answer:''}
+    ]
+}
+
+const getters={
+    
+}
+
+const actions={
+
+}
+
+const mutations={
+
+}
+
+export default{
+    namespace:true,
+    state,
+    getters,
+    actions,
+    mutations
+}
---
+++ frontend/src/store/modules/user.js
@@ -0,0 +1,24 @@
+
+const state={
+    userName:'',
+    password:''
+}
+
+const getters={
+
+}
+
+const actions={
+
+}
+
+const mutations={
+
+}
+export default{
+    namespace:true,
+    state,
+    getters,
+    actions,
+    mutations
+}
