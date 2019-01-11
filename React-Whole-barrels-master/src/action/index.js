cc752c7720b0cc5c54166344dff46242a202c162
gao.ling <gao.ling@ctrip.com>
2019-01-11 14:17:15 +0800

    commit

--- React-Whole-barrels-master/src/action/index.js
+++
@@ -1,8 +0,0 @@
-
-
-export  const save_login_info=(data)=>{
-    return {type:'SAVE_LOGIN_INFO',loginInfo:data}
-};
-export  const save_tally_book=(data)=>{
-    return {type:'SAVE_TALLY_BOOK',tallyBook:data}
-};
