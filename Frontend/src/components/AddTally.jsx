0049a150988a89c6306bfc3deab77e8016468721
gao.ling <gao.ling@ctrip.com>
2019-01-11 17:33:19 +0800

    commit

--- Frontend/src/components/AddTally.jsx
+++ Frontend/src/components/AddTally.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
+import { Form, Radio, Icon } from 'antd'
 
-class AddTally extends React.Component{
+class AddTally extends React.Component {
 
-    render(){
-        const {loginInfo}=this.props;
-        console.log(loginInfo)
+    render() {
+        // const {data}=this.props;
+        const { getFieldDecorator } = this.props.form;
+        const FormItem = Form.Item;
+        const formItemLayout = {
+            labelCol: { span: 6 },
+            wrapperCol: { span: 14 },
+        };
+        const data = {
+            qname: 'queque:',
+            qid: 123,
+            qoption: ['1', '2', '3', '4']
+        }
         return (
             <div>
-                <h2>{loginInfo.userName}</h2>
-                <h1>{loginInfo.password}</h1>
+                <Form onSubmit={this.handleSubmit}>
+                    <FormItem {...formItemLayout}>
+                        <p>{data.qname}</p>
+                        {qid = data.qid && getFieldDecorator({ qid })(
+                            <Radio.Group>
+                                {data.qoption.map(o=><Radio value={o}>{o}</Radio>)}
+                            </Radio.Group>
+                        )}
+                    </FormItem>
+                </Form>
             </div>
         );
     }
